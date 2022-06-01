086f912863d20f0b41da163033db8a0b4fd669e9
Stefano Taillefert <stefano.taille@gmail.com>
2022-06-01 18:50:30 +0200

    Modularize data, add spaces beta

---
+++ data.py
@@ -0,0 +1,16 @@
+talents = ["PomuRainpuff", "EliraPendora", "FinanaRyugu"]
+talents += [
+    "Petra_Gurin",
+    "Rosemi_Lovelock",
+    "Selen_Tatsuki",
+]
+talents += ["EnnaAlouette", "ReimuEndou", "NinaKosaka", "MillieParfait"]
+talents += [
+    "luca_kaneshiro", "Vox_Akuma", "Mysta_Rias", "ike_eveland", "shu_yamino"
+]
+talents += [
+    "sonny_brisko", "Fulgur_Ovid", "Yugo_Asuma", "uki_violeta", "alban_knox"
+]
+
+schedule_keywords = ["schedule", "#PetraOnAir", "weekly"]
+guerrilla_keywords = ["guerrilla", "guerilla"]
--- fetcher.py
+++ fetcher.py
@@ -2,6 +2,8 @@ from os import getenv
 
 from tweepy import Client
 
+from data import guerrilla_keywords, schedule_keywords, talents
+
 # Separator between tweets
 separator = "---------------------------------"
 # Twitter bearer token
@@ -11,35 +13,24 @@ if bearer_token == None:
     raise ValueError("Twitter bearer token not found!")
 client = Client(bearer_token, wait_on_rate_limit=True)
 
+
 # Matches tweets that:
-# - have the keyword "schedule" or "weekly" in them and have an image attached
-#   OR have the keyword "guerrilla" or "guerilla" in them
+# - have a keyword from schedule_keywords in them and have an image attached
+#   OR have a keyword from guerrilla_keywords in them
 # - are not retweets
-# - are from the talents below
-
-talents = ["PomuRainpuff", "EliraPendora", "FinanaRyugu"]
-talents += [
-    "Petra_Gurin",
-    "Rosemi_Lovelock",
-    "Selen_Tatsuki",
-]
-talents += ["EnnaAlouette", "ReimuEndou", "NinaKosaka", "MillieParfait"]
-talents += [
-    "luca_kaneshiro", "Vox_Akuma", "Mysta_Rias", "ike_eveland", "shu_yamino"
-]
-talents += [
-    "sonny_brisko", "Fulgur_Ovid", "Yugo_Asuma", "uki_violeta", "alban_knox"
-]
-
-query = "-is:retweet ((guerrilla OR guerilla) OR ((schedule OR #PetraOnAir OR weekly) has:media)) (from:"
-query += " OR from:".join(talents)
-query += ")"
-
-
-def fetch_tweets(newest_id):
+# - are from someone in the talents array
+def fetch_tweets(newest_id, talents):
+    user_names = []
+    for user in talents:
+        user_names.append(user.username)
+    query = "-is:retweet ((" + " OR ".join(
+        guerrilla_keywords) + ") OR ((" + " OR ".join(
+            schedule_keywords) + ") has:media)) (from:"
+    query += " OR from:".join(user_names)
+    query += ")"
     response = client.search_recent_tweets(query,
                                            since_id=newest_id,
-                                           max_results=len(talents),
+                                           max_results=len(user_names),
                                            expansions=["author_id"])
 
     new_tweets = response.meta["result_count"]
@@ -48,11 +39,18 @@ def fetch_tweets(newest_id):
     return [response, new_tweets, newest_id]
 
 
-if __name__ == "__main__":
-    [response, tweets_fetched, newest_id] = fetch_tweets(None)
-    tweets = response.data
-    users = {user["id"]: user for user in response.includes["users"]}
-    print(tweets_fetched, "found\n")
-    for tweet in tweets:
-        print("Tweet from {0} - https://twitter.com/twitter/statuses/{1}\n{2}".
-              format(users[tweet.author_id].username, tweet.id, separator))
+# Matches spaces that:
+# - are from the talents array
+def fetch_spaces(talents):
+    user_ids = []
+    for user in talents:
+        user_ids.append(user.id)
+    response = client.get_spaces(user_ids=user_ids, expansions=["creator_id"])
+    new_spaces = response.meta["result_count"]
+    return [response, new_spaces]
+
+
+def fetch_user_ids():
+    usernames = ",".join(talents)
+    response = client.get_users(usernames=usernames).data
+    return [response, len(response)]
